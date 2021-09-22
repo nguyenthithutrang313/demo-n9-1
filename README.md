1a5af3d58670b00f090444e9be3af8ab13ce42d1
Huyen1204 <nguyenhuyen98pm@gmail.com>
2021-09-22 11:45:47 +0700

    2002xxxx Huyen edited

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 git add <ten_file> / .
 git commit -m "detail message"
 git push
+
+## branches
+1. master -> chạy ổn nhất
+2. dev  (sửa conflix)
+3. dev-huyen, feature/login
--- src/main/java/HelloIntellij.java
+++ src/main/java/HelloIntellij.java
@@ -4,6 +4,6 @@ public class HelloIntellij {
      * @param args arguments.
      */
     public static void main(String[] args) {
-        System.out.println("Hello Intellij");
+        System.out.println("Hello Huyennnnn");
     }
 }
